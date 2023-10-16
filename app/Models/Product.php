512c1ba1ecdd09aa427531e0a6d7682d5cfbff47
Gustafa Bayu <faafa13@gmail.com>
2023-10-16 20:02:24 +0700

    Initial Project REST

---
+++ app/Models/Product.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use MongoDB\Laravel\Eloquent\Model as EloquentModel;
+
+class Product extends EloquentModel
+{
+    use HasFactory;
+}
